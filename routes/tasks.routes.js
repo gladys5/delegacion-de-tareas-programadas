b423a4fcdc5e2ab12930254c41b9f9584e1af8bb
gladys5 <yumanji@live.com>
2022-06-28 19:04:51 -0600

    repairRouter

--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -12,8 +12,8 @@ const { validationDate } = require("../middelwares/validationDate.midellware");
 
 router.post("/", createTask, validationDate);
 router.get("/", getTasksRegistrate);
-router.get("/:status/", getTaskBystatus);
-router.patch("/:id", updateTaskById, validationDate);
+router.get("/:status", getTaskBystatus);
+router.patch("/:id", validationDate, updateTaskById);
 router.delete("/:id", validationDate, cancelTask);
 
 module.exports = { TaskRouter: router };
